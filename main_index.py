664aee54dfb725103a3c58123cf85c0f2b21297a
antonio waghtom <waghtompereira@gmail.com>
2021-12-05 13:16:33 -0300

    janela
    dimensionar de forma centralizada na janela

--- main_index.py
+++ main_index.py
@@ -1,7 +1,21 @@
 import tkinter as tk
 
-app = tk.Tk()
 
-app.geometry('200x200')
+root = tk.Tk() # create a Tk root window
 
-app.mainloop()
+w = 800 # width for the Tk root
+h = 650 # height for the Tk root
+
+# get screen width and height
+ws = root.winfo_screenwidth() # width of the screen
+hs = root.winfo_screenheight() # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+# set the dimensions of the screen 
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+root.mainloop() # starts the mainloop
