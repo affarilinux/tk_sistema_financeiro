24214d746518f87066fbcaea5fec735b405c5b50
antonio waghtom <waghtompereira@gmail.com>
2021-12-13 23:24:45 -0300

    problema home e configurações atualizart

--- main_index.py
+++ main_index.py
@@ -163,7 +163,7 @@ class ClassBanco():
 ###################################################
 class BancoExecucao():
 
-    ############################################### inserir dados
+    ############################################### inserir if none
     def funcao_classdb_inserir_nova_alianca(self):
                            
         self.sql_cursorr.execute( "INSERT INTO Instituicao VALUES (1, '"+NOME_NOVA_ALIANCA+"')")
@@ -172,6 +172,11 @@ class BancoExecucao():
     def funcao_cursorrdb_inserir_thue(self):
 
         self.sql_cursorr.execute("INSERT INTO Processos  VALUES (1, 'Thue')")
+        self.funcao_classdb_commit()
+
+    def funcao_db_inserir_instituicao_linha2_none (self):
+        
+        self.sql_cursorr.execute( "INSERT INTO Instituicao VALUES (2, '"+NOME_NOVA_ALIANCA+"')")
         self.funcao_classdb_commit()
 
     ############################################### visualizar dados
@@ -204,6 +209,16 @@ class BancoExecucao():
 
             self.funcao_cursorrdb_inserir_thue()
 
+    def funcao_db_visualiza_instituicao_linha2(self):
+
+        self.sql_cursorr.execute( "SELECT nome_igreja FROM Instituicao WHERE cod = 2")
+        self.visualiza_L2 = self.sql_cursorr.fetchone()
+
+        if self.visualiza == None:
+
+            self.funcao_db_inserir_instituicao_linha2_none ()
+
+    ############################################### upgrade
     def funcao_db_update_tbprocessos_linha1(self):
 
         self.funcao_classdb_conectar()
@@ -412,20 +427,25 @@ class ConfiguracaoDestruirWidgetSSalvar():
 
         self.botao_salvar_instituicao.destroy()
 
-################################################### if-else
+################################################### if-else  home
     def funcao_class_if_destruir_widget(self):
-
-        self.LABEL_BANCO_INSTITUICAO_FIXA.destroy()
+        
+        self.LABEL_BANCO_INSTITUICAO_FIXA.destroy() #Varivel fixa
 
         self.funcao_db_conectar_e_visualizar_1()
+        destroir_banco_1 = self.visualiza [0]
 
-        destroir_banco_1 = self.visualiza[0]
+        self.funcao_db_visualiza_instituicao_linha2()
+        destruir_banco_2 = self.visualiza_L2 [0]
         
-        if destroir_banco_1 != NOME_NOVA_ALIANCA: 
+        
+        if destroir_banco_1 != destruir_banco_2: 
+            print(2)
                   
             self.funcao_destruir_atalizar_label()
 
-        elif destroir_banco_1 == NOME_NOVA_ALIANCA:
+        elif destroir_banco_1 == destruir_banco_2: 
+            print(1)
             
             self.funcao_destruir_configuracao_entry_salvar()
                     
@@ -530,6 +550,7 @@ class MenuWidget ( ClassBanco,barraVisualizarNomeInstituicao, BarraMenuHome,
         self.funcao_classdb_conectar()
         self.funcao_classdb_criar_tabela()   
         self.funcao_classdb_visualizar_1()  
+        self.funcao_db_visualiza_instituicao_linha2()
         self.funcao_db_visualiar_tbprocessos()
         self.funcao_db_update_tbprocessos_linha1()
         self.funcao_classdb_desconectar()
